53eccc16289c7c3d428b924bf2a615df417cc799
raksa <eng.raksa@gmail.com>
2017-04-30 19:41:32 +0700

    with navigator

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,43 @@
-import {
-  AppRegistry
-} from 'react-native';
 
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+  Text
+} from 'react-native';
+import { TabNavigator } from 'react-navigation';
 import SocketIOExample from './client/SocketIOExample.component';
 
-AppRegistry.registerComponent('SocketIOExample', () => SocketIOExample);
+class Second extends Component {
+  static navigationOptions = {
+    title: 'Third tab',
+  };
+  render() {
+    return (
+      <View>
+        <Text>This is second tab</Text>
+      </View>
+    );
+  }
+}
+
+class Third extends Component {
+  static navigationOptions = {
+    title: 'Third tab',
+  };
+  render() {
+    return (
+      <View>
+        <Text>This is third tab</Text>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = TabNavigator({
+  Main: {screen: SocketIOExample},
+  Second: {screen: Second},
+  Third: {screen: Third}
+});
+
+AppRegistry.registerComponent('SocketIOExample', () => SimpleApp);
